e2bf3d33d3e7a129408d80a3ad7c2dac6d5e1562
Dobrica Pavlinusic <dpavlin@rot13.org>
2016-09-19 17:29:25 +0200

    use FROM env

--- grep-maillog.sh
+++ grep-maillog.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -xe
-top_from=`./mailq-count-from.sh | sort -n -r | head -1 | awk '{ print $2 }'`
-sudo grep $* $top_from /var/log/mail.log
+test -z "$FROM" && FROM=`./mailq-count-from.sh | sort -n -r | head -1 | awk '{ print $2 }'`
+sudo grep $* $FROM /var/log/mail.log
