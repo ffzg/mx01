8c3fb2469042e224eb0b6a0d512fe7f5b341f6ab
Dobrica Pavlinusic <dpavlin@rot13.org>
2016-09-19 14:22:38 +0200

    optionally specify from address, default is MAILER-DAEMON

--- remove-mailer-daemon.sh
+++ remove-mailer-daemon.sh
@@ -1,1 +1,5 @@
-mailq | grep MAILER-DAEMON | grep ^[0-9A-Z] | awk '{ print $1 }' | sed 's/*$//'
+# usage: ./remove-mailer-daemon.sh [MAILER-DAEMON] | sudo postsuper -d -
+
+from=MAILER-DAEMON
+test ! -z "$1" && from=$1
+mailq | grep $from | grep ^[0-9A-Z] | awk '{ print $1 }' | sed 's/*$//'
