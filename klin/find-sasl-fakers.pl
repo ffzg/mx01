e49b769dfae07ce83e19d08a3735d08ee1d3a570
Dobrica Pavlinusic <dpavlin@rot13.org>
2016-10-11 11:50:59 +0200

    find users which are logged in but send from different e-mail

---
+++ klin/find-sasl-fakers.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use autodie;
+
+# ./find-sasl-fakers.pl `grep -l sasl_ ./log.d/*/*.log`
+
+use Data::Dump qw(dump);
+
+my $aliases;
+open(my $a, '<', 'log.d/mudrac/aliases');
+while(<$a>) {
+	next if ( /^\s*#/ );
+	if ( /^(\S+):\s*(\S+)/ ) {
+		my ( $full_name, $login ) = ( $1, $2 );
+		$aliases->{$full_name} = $login;
+	}
+}
+close($a);
+
+my $spam;
+my $ids;
+
+my $regex = 'X';
+
+while(<>) {
+	chomp;
+
+	next if m{postfix/cleanup};
+
+	if ( /\s(\w+): client=(\S+), .+ sasl_username=(\S+)/ ) {
+		my ( $id, $client, $username ) = ( $1,$2,$3 );
+		$username =~ s/\@ffzg.hr//;
+		$ids->{$id} = [ $username, $client ];
+		$regex = join('|', keys %$ids);
+	} elsif ( /\s($regex): .+?from=<([^>]+)>/ ) {
+		my ( $id, $from ) = ( $1, $2 );
+		$from =~ s/\@ffzg.hr//;
+		if ( my $login = $aliases->{$from} ) {
+			$from = $login;
+		}
+
+		next if ! defined $ids->{$id};
+		my $login = $ids->{$id}->[0];
+
+		if ( $from eq $login ) {
+			#warn "# OK $id $from ",dump( $ids->{$id} );
+			print STDERR ".";
+			delete $ids->{$id};
+		} else {
+			#warn "SPAM $id $from ",dump( $ids->{$id}, $_ );
+			print STDERR "S";
+			$spam->{$login}++;
+		}
+	}
+}
+
+#warn "# ids ",dump($ids);
+warn "# spam ",dump($spam);
