d34ec1eb0c75a1667400b328a3f57b07710bd8c9
Dobrica Pavlinusic <dpavlin@rot13.org>
2017-04-11 12:28:59 +0200

    use postcat to access queue

--- mailq-php-script.sh
+++ mailq-php-script.sh
@@ -1,5 +1,9 @@
-mailq | grep ^[0-9A-Z] | awk '{ print $1 }' \
+mailq | grep ^[0-9A-Z] | awk '{ print $1 }' | sed 's/*$//' \
 | sed 's!\(.\)\(.\)\(.\)!/var/spool/postfix/deferred/\1/\2/\3/\1\2\3!' \
-| xargs -i sudo grep -a X-PHP-Originating-Script {} \
-| tr '[\000-\011\013-\037\177-\377]' '\n' \
+| xargs -i sudo grep -a -l X-PHP-Originating-Script {} \
+| cut -d/ -f 9 \
+| xargs -i sudo postcat -q {} \
+| cat
+exit 0
 | grep X-PHP-Originating-Script
+
