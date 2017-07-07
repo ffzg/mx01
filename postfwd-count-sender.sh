77ab9adff6c4d4d694c8fb102f6a8b8df1810c63
Dobrica Pavlinusic <dpavlin@rot13.org>
2017-07-07 08:04:37 +0200

    show also recipient with any arg, and specify LOG

---
+++ postfwd-count-sender.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -xe
+
+test -z "$LOG" && LOG=/var/log/mail.log
+
+recipient="#"
+test ! -z "$1" && recipient='s/, recipient=/ /'
+grep 'postfwd.*user=' $LOG | sed -e 's/^.*sender=//' -e "$recipient" -e 's/,.*//' -e 's/[<>]//g'| sort | uniq -c | sort -nr | column -t
