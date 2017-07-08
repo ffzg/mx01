5b63f7cf9289acb6f5c082c2896c7532885df16e
Dobrica Pavlinusic <dpavlin@rot13.org>
2017-07-08 17:04:53 +0200

    fix grep to work without sasl login

--- postfwd-count-sender.sh
+++ postfwd-count-sender.sh
@@ -4,4 +4,4 @@ test -z "$LOG" && LOG=/var/log/mail.log
 
 recipient="#"
 test ! -z "$1" && recipient='s/, recipient=/ /'
-grep 'postfwd.*user=' $LOG | sed -e 's/^.*sender=//' -e "$recipient" -e 's/,.*//' -e 's/[<>]//g'| sort | uniq -c | sort -nr | column -t
+grep 'postfwd.*sender=' $LOG | sed -e 's/^.*sender=//' -e "$recipient" -e 's/,.*//' -e 's/[<>]//g'| sort | uniq -c | sort -nr | column -t
