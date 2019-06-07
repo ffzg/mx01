37dd83bf966a6259fdb17ea97f2c1ddbc6cda41c
Dobrica Pavlinusic <dpavlin@rot13.org>
2019-06-07 13:22:12 +0200

    store output in /dev/shm/

--- count-user-mails.sh
+++ count-user-mails.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -e
 
-ls /var/log/mail.log-*.gz | while read log ; do
+(
+ls /var/log/mail.log-* | while read log ; do
 	echo "# $log"
-	zgrep user= $log | grep sender= | grep -v 193.198.21[2-5] | sed -e 's/^.*user=//' -e 's/,.*$//' | sort | uniq -c | sort -rn | head -3
+	zgrep user= $log | grep sender= | grep -v 193.198.21[2-5] | sed -e 's/^.*user=//' -e 's/,.*$//' | sort | uniq -c | sort -rn | head -5
 done
+) | tee /dev/shm/$( basename $0 ).txt
 
