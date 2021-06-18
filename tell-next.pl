40cff792d79fd4354be51cd0a7c0cab8470197a3
Dobrica Pavlinusic <dpavlin@rot13.org>
2021-06-18 07:30:34 +0200

    added tell-next.pl

---
+++ tell-next.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use autodie;
+
+my ($command,$file) = @ARGV;
+die "usage: $0 /bin/cat /var/log/mail.log" unless -x $command && -r $file;
+
+open(my $fh, '<', $file);
+my $tell = $command . $file;
+$tell =~ s/\W//g;
+$tell = "/dev/shm/tell.$tell";
+if ( -e $tell && ! $ENV{DEBUG} ) {
+	open(my $fh_tell, '<', $tell);
+	my $pos = <$fh_tell>;
+	if ( $pos < -s $file ) {
+		eval {
+			warn "# $file seek $pos\n";
+			seek($fh, $pos, 0);
+		}
+	} else {
+		warn "# $file from beginning";
+	}
+}
+
+open(my $fh_command, '|-', $command);
+while(<$fh>) {
+	print $fh_command $_;
+}
+
+open(my $fh_tell, '>', $tell);
+print $fh_tell tell($fh);
+close($fh_tell);
+
