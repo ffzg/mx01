36d7625414034de883697eee740dd159e9ff93b7
Dobrica Pavlinusic <dpavlin@rot13.org>
2021-11-25 12:47:14 +0100

    fix support for multiple files as args

--- maillog-delay.sh
+++ maillog-delay.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-test -z $DELAY && DELAY=20
+test -z "$DELAY" && DELAY=20
 
-zgrep delays= $( test ! -z "$@" && echo $@ || echo /var/log/mail.log ) | grep -v status=deferred | sed -e 's/postfix.*delay=//' -e 's/, delays=/ /' -e 's/, dsn=.*$//' | awk -v DELAY=$DELAY '{ if ( $5 > DELAY ) print }'
+zgrep delays= $( test ! -z "$1" && echo $@ || echo /var/log/mail.log ) | grep -v status=deferred | sed -e 's/postfix.*delay=//' -e 's/, delays=/ /' -e 's/, dsn=.*$//' | awk -v DELAY=$DELAY '{ if ( $5 > DELAY ) print }'
