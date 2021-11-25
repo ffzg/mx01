66709445d737c8e54fae7312180fc626cddcdad1
Dobrica Pavlinusic <dpavlin@rot13.org>
2021-11-25 12:43:19 +0100

    specify delay in env: DELAY=10 ./maillog-delay.sh

--- maillog-delay.sh
+++ maillog-delay.sh
@@ -1,1 +1,5 @@
-zgrep delays= $( test ! -z "$@" && echo $@ || echo /var/log/mail.log ) | grep -v status=deferred | sed -e 's/postfix.*delay=//' -e 's/, delays=/ /' -e 's/, dsn=.*$//' | awk '{ if ( $5 > 20 ) print }'
+#!/bin/sh
+
+test -z $DELAY && DELAY=20
+
+zgrep delays= $( test ! -z "$@" && echo $@ || echo /var/log/mail.log ) | grep -v status=deferred | sed -e 's/postfix.*delay=//' -e 's/, delays=/ /' -e 's/, dsn=.*$//' | awk -v DELAY=$DELAY '{ if ( $5 > DELAY ) print }'
