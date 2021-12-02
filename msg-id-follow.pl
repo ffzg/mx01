82243f10ab41630c887b89c89e409f50207ab304
Dobrica Pavlinusic <dpavlin@rot13.org>
2021-12-02 11:51:38 +0100

    use /var/log/mail.log if we don't have pipe or arguments

--- msg-id-follow.pl
+++ msg-id-follow.pl
@@ -2,6 +2,11 @@
 use warnings;
 use strict;
 
+# if we don't have pipe or arguments
+if ( -t STDIN and not @ARGV ) {
+	push @ARGV, '/var/log/mail.log';
+}
+
 open(my $ids_fh, '<', '/dev/shm/msg.ids') || die "/dev/shm/msg.ids: $!";
 my @ids = map { s/[\r\n]$//; $_ } <$ids_fh>;
 close($ids_fh);
