afd222c94e64843284e53681e069120709b77f23
Dobrica Pavlinusic <dpavlin@rot13.org>
2021-12-02 11:34:16 +0100

    follow message ids from /dev/shm/msg.ids
    Display all related messages

---
+++ msg-id-follow.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+open(my $ids_fh, '<', '/dev/shm/msg.ids') || die "/dev/shm/msg.ids: $!";
+my @ids = map { s/[\r\n]$//; $_ } <$ids_fh>;
+close($ids_fh);
+
+my $id_regex = join('|', @ids);
+
+warn "# got ", scalar(@ids), " message ids $id_regex\n";
+
+while(<>) {
+	chomp;
+	if ( m/: ($id_regex):/ ) {
+		print "$_\n";
+		if ( m/queued as ([0-9A-F]+)/ ) {
+			$id_regex .= '|' . $1;
+			#warn "## ++ id_regex: $id_regex\n";
+		}
+		if ( m/: ([0-9A-F]+): removed/ ) {
+			$id_regex =~ s/$1\|//;
+			#warn "## -- id_regex: $id_regex\n";
+		}
+	}
+}
