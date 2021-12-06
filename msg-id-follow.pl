b5d6697d2a2f69136f5815f2d0ba41c4ab31be84
Dobrica Pavlinusic <dpavlin@rot13.org>
2021-12-06 20:29:42 +0100

    ignore invalid message ids

--- msg-id-follow.pl
+++ msg-id-follow.pl
@@ -2,6 +2,8 @@
 use warnings;
 use strict;
 
+my $debug = $ENV{DEBUG} || 0;
+
 # if we don't have pipe or arguments
 if ( -t STDIN and not @ARGV ) {
 	push @ARGV, '/var/log/mail.log';
@@ -21,16 +23,20 @@ while(<>) {
 	if ( m/($id_regex)/ ) {	# everything with this queue id
 		print "$_\n";
 		if ( m/queued as ([0-9A-F]+)/ ) {
-			$id_regex .= '|' . $1;
-			#warn "## ++ id_regex: $id_regex\n";
+			if ( length($1) >= 7 ) { # minimum valid message id
+				$id_regex .= '|' . $1;
+				warn "## ++ queued id_regex: $1\n" if $debug;
+			} else {
+				warn "# ignore $1 not valid message id\n";
+			}
 		}
 		if ( m/forwarded as ([0-9A-F]+)/ ) {
 			$id_regex .= '|' . $1;
-			#warn "## ++ id_regex: $id_regex\n";
+			warn "## ++ orwarded id_regex: $1\n" if $debug;
 		}
 		if ( m/: ([0-9A-F]+): removed/ ) {
+			warn "## -- removed id_regex: $1\n" if $debug;
 			$id_regex =~ s/$1\|//;
-			#warn "## -- id_regex: $id_regex\n";
 		}
 	}
 }
