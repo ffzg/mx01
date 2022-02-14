5227e16800d817f9629c7ca6bb4c7e2ca5f42bd1
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-02-14 07:22:57 +0100

    count external senders

---
+++ count-sender-mails.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+
+# filter just for one user:
+# $0 grep karijere
+
+# filter just some files by date:
+# DATE=202111 ./count-user-mails.sh
+
+LIMIT="head -5"
+test ! -z "$1" && LIMIT="$@"
+
+(
+ls /var/log/mail.log-$DATE* | while read log ; do
+	echo "# $log"
+	zgrep postfwd $log | grep -v user= | grep sender= | grep -v 'sender=<[^>]*ffzg.hr>' | sed -e 's/^.*sender=//' -e 's/,.*$//' | sort | uniq -c | sort -rn | $LIMIT || true
+	# || true is here to keep processing files in grep in LIMIT didn't find anything
+done
+) | tee /dev/shm/$( basename $0 ).txt
+
