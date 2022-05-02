ad65be952d1260435af404ad5b14729f3ae51f9a
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-05-02 09:06:17 +0200

    LOG=/var/log/mail.log-*.gz ./msg-id-grep.sh pattern

--- msg-id-grep.sh
+++ msg-id-grep.sh
@@ -1,3 +1,12 @@
 #!/bin/sh -xe
 
+if [ ! -z "$LOG" ] ; then
+	log_file=/tmp/$( basename $LOG )
+	zcat $LOG > $log_file
+	zgrep postfix $log_file | grep "$@" | tee /dev/stderr | sed -e 's/^.*\]: \([0-9A-F]*\):.*$/\1/' | sort -u > /dev/shm/msg.ids
+	./msg-id-follow.pl $log_file | less -S
+	exit
+fi
+
+
 grep postfix /var/log/mail.log | grep "$@" | tee /dev/stderr | sed -e 's/^.*\]: \([0-9A-F]*\):.*$/\1/' | sort -u > /dev/shm/msg.ids
