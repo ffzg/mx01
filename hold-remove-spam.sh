a3f04e5d871ab81357439803cb3a81c87d7f8d7a
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-10-27 18:48:18 +0200

    better hold patterns

--- hold-remove-spam.sh
+++ hold-remove-spam.sh
@@ -1,7 +1,7 @@
-#!/bin/sh -xe
+#!/bin/sh -e
 
-sudo id
+sudo id >/dev/null
 mailq | grep '^[0-9A-Z]*!' | awk '{ print $1 }' | sed 's/!$//' | while read id ; do
-	echo "### $id ###"
-	sudo find /var/spool/postfix/ -name $id | sudo xargs -i postcat {} | tee /dev/stderr | sed "s/^/$id /" | grep -E '(messages are queued in the server due to error.)' | cut -d' ' -f1 | sudo postsuper -d -
+	#echo "### $id ###"
+	sudo find /var/spool/postfix/ -name $id | sudo xargs -i postcat {} | sed "s/^/$id /" | grep -E '(messages are queued in the server due to error.|Mailbox Full|storageapi.fleek.co|support@posta.hr|service@intl.paypl.com|newsletter@intersport.hr|cutt.ly/5NrSmeX|Ferzi Karhan|imate isto prezime kao i moj pokojni klijent|Ujedinjenih naroda .UN. na razini Prevaranti / Prevaranti)' | cut -d' ' -f1 | sudo postsuper -d -
 done
