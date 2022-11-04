651f21962a2c35af69020bf63907b9eb61e5aff5
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-11-04 12:49:26 +0100

    update from master

--- msg-id-follow.pl
+++ msg-id-follow.pl
@@ -2,13 +2,18 @@
 use warnings;
 use strict;
 
+use Data::Dumper;
+
+my $debug = $ENV{DEBUG} || 0;
+
 # if we don't have pipe or arguments
 if ( -t STDIN and not @ARGV ) {
 	push @ARGV, '/var/log/mail.log';
 }
 
 open(my $ids_fh, '<', '/dev/shm/msg.ids') || die "/dev/shm/msg.ids: $!";
-my @ids = map { s/[\r\n]$//; $_ } <$ids_fh>;
+my $id_usage;
+my @ids = map { s/[\r\n]$//; $id_usage->{$_} = 1; $_ } <$ids_fh>;
 close($ids_fh);
 
 my $id_regex = join('|', @ids);
@@ -17,15 +22,31 @@ warn "# got ", scalar(@ids), " message ids $id_regex\n";
 
 while(<>) {
 	chomp;
-	if ( m/: ($id_regex):/ ) {
+	#if ( m/: ($id_regex):/ ) { # only postfix lines
+	if ( m/($id_regex)/ ) {	# everything with this queue id
+		$id_usage->{$1}++;
+		print "[$1] " if $debug;
 		print "$_\n";
 		if ( m/queued as ([0-9A-F]+)/ ) {
-			$id_regex .= '|' . $1;
-			#warn "## ++ id_regex: $id_regex\n";
+			if ( length($1) >= 7 ) { # minimum valid message id
+				$id_usage->{$1}++; undef $id_regex;
+				warn "## ++ queued id_regex: $1\n" if $debug;
+			} else {
+				warn "# ignore $1 not valid message id\n";
+			}
+		}
+		if ( m/forwarded as ([0-9A-F]+)/ ) {
+			$id_usage->{$1}++; undef $id_regex;
+			warn "## ++ orwarded id_regex: $1\n" if $debug;
 		}
 		if ( m/: ([0-9A-F]+): removed/ ) {
-			$id_regex =~ s/$1\|//;
-			#warn "## -- id_regex: $id_regex\n";
+			warn "## -- removed id_regex: $1\n" if $debug;
+			delete $id_usage->{$1}; undef $id_regex;
 		}
+
+		$id_regex = join('|', keys %$id_usage) if ( ! $id_regex );
+		last if $id_regex eq '';
 	}
 }
+
+warn "# left ",Dumper( $id_usage );
