ee953fb8a8577fae66ab9ba6d3e167338956cff1
FFZG admin <admin@ffzg.hr>
2022-11-07 12:37:17 +0100

    more spam

--- hold-remove-spam.sh
+++ hold-remove-spam.sh
@@ -3,5 +3,5 @@
 sudo id >/dev/null
 mailq | grep '^[0-9A-Z]*!' | awk '{ print $1 }' | sed 's/!$//' | while read id ; do
 	#echo "### $id ###"
-	sudo find /var/spool/postfix/ -name $id | sudo xargs -i postcat {} | sed "s/^/$id /" | grep -E '(messages are queued in the server due to error.|Mailbox Full|storageapi.fleek.co|support@posta.hr|service@intl.paypl.com|newsletter@intersport.hr|cutt.ly/5NrSmeX|Ferzi Karhan|imate isto prezime kao i moj pokojni klijent|Ujedinjenih naroda .UN. na razini Prevaranti / Prevaranti)' | cut -d' ' -f1 | sudo postsuper -d -
+	sudo find /var/spool/postfix/ -name $id | sudo xargs -i postcat {} | sed "s/^/$id /" | grep -E '(messages are queued in the server due to error.|Mailbox Full|storageapi.fleek.co|support@posta.hr|service@intl.paypl.com|newsletter@intersport.hr|cutt.ly/5NrSmeX|Ferzi Karhan|imate isto prezime kao i moj pokojni klijent|Ujedinjenih naroda .UN. na razini Prevaranti / Prevaranti|kontaktirao po ovom pitanju jer se prezivate|croloadredey-bd508c.ingress-earth.ewp.live/siudaaa|managed-vps.net|Admin@ffzg.hr)' | cut -d' ' -f1 | sudo postsuper -d -
 done
