42ac7c92ff691eb8a3f3048e8251955a38a2c4dd
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-11-08 09:02:37 +0100

    remove -x

--- postfwdspam-dumpall.sh
+++ postfwdspam-dumpall.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -xe
+#!/bin/sh -e
 
 cat <<__SQL__ | mysql --user=postfwdspam --password=$(grep 'my $password' /etc/postfix/postfwd-plugins/postfwd-anti-spam.plugin  | cut -d\" -f2) $1 postfwdspam
 select * from postfwd_logins
