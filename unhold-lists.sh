ad417f46efac0b30759d8195e64e3aa1af42c6ad
FFZG admin <admin@ffzg.hr>
2022-11-08 11:58:50 +0100

    unhold by from or to in mailq output

--- unhold-lists.sh
+++ unhold-lists.sh
@@ -1,4 +1,9 @@
 #!/bin/sh -e
 
-mailq | grep '^[0-9A-F]*!' | grep -E '(drupaladmin@mail.h-net.org|owner-museum-l@HOME.EASE.LSOFT.COM|visionesdelofantastico@gmail.com|@hotmail.com|jtomas@ffzg.hr|bounce@client.scholargatherings.com|owner-digital-preservation@JISCMAIL.AC.UK|info.icarushr@gmail.com|owner-infoling@LISTSERV.REDIRIS.ES|bounce-mc)' | awk '{ print $1 }' | tr -d '!' | xargs -i postsuper -H {}
+# -B 1 to support to address from mailq
+
+mailq \
+	| grep -B 1 -E '(drupaladmin@mail.h-net.org|owner-museum-l@HOME.EASE.LSOFT.COM|visionesdelofantastico@gmail.com|@hotmail.com|jtomas@ffzg.hr|bounce@client.scholargatherings.com|owner-digital-preservation@JISCMAIL.AC.UK|info.icarushr@gmail.com|owner-infoling@LISTSERV.REDIRIS.ES|bounce-mc)' \
+	| grep '^[0-9A-F]*!' \
+	| awk '{ print $1 }' | tr -d '!' | xargs -i postsuper -H {}
 sendmail -q
