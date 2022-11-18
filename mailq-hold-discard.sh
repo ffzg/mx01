0035e8abc07700156b130a23037aff0dd70b210c
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-11-18 13:44:10 +0100

    process one by one

--- mailq-hold-discard.sh
+++ mailq-hold-discard.sh
@@ -15,8 +15,6 @@ write_discard() {
 	echo "/$patt/	DISCARD" | tee -a $file
 
 	./hold-remove-grep.sh $patt
-
-	exit 1;
 }
 
 echo "# top 5 from" > /dev/stderr
@@ -24,7 +22,8 @@ most_from=$( mailq | grep '^[0-9A-F]' | sort -k 7 | grep \! | awk '{ print $7 }'
 
 echo "## most_from $most_from"
 
-mailq | grep '^[0-9A-F]' | sort -k 7 | grep \! | grep $most_from | while read line ; do
+#mailq | grep '^[0-9A-F]' | sort -k 7 | grep \! | grep $most_from | while read line ; do
+line=$( mailq | grep '^[0-9A-F]' | grep \! | grep $most_from )
 
 	id=$( echo $line | cut -d\! -f1 )
 
@@ -33,18 +32,26 @@ mailq | grep '^[0-9A-F]' | sort -k 7 | grep \! | grep $most_from | while read li
 
 	sudo find /var/spool/postfix/ -name $id | sudo xargs -i postcat {} > $id_file
 	reply_to=$( grep '^Reply-To: .*' $id_file | cut -d: -f2 )
+	echo "## replay_to $replay_to"
+
 	if [ ! -z "$reply_to" ] ; then
-		less $id_file
+		less -P "DISCARD reply to $reply_to" $id_file
 		write_discard "$reply_to" # quote to pass as signle arg
 		echo $id | tee $discard_file.id
 
 	else
-		less $id_file
-		echo -n "ENTER anything to discard "
-		read do_discard < /dev/stdin
-		test ! -z "$do_discard" && write_discard "$do_discard" ".body"
+		less -P "find pattern" $id_file
 	fi
 
+		read -p "ENTER anything and press ENTER to discard (ctrl+c to abort) [$sender] " do_discard < /dev/stdin
+		test ! -z "$do_discard" && write_discard "$do_discard" ".body"
+		echo "unhold	postsuper -H $id"
+		echo "delete	postsuper -d $id"
+
+		sender=$( grep '^sender: ' $id_file | grep '[0-9][0-9]*@gmail.com' | cut -d' ' -f2 )
+		write_discard "$sender"
 
 
-done
+
+
+#done
