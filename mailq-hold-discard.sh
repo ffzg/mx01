df48cec306f4dff785f18bc41196f59c4b2fcd30
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-11-18 11:27:02 +0100

    create discard files from hold mailq

---
+++ mailq-hold-discard.sh
@@ -0,0 +1,42 @@
+#!/bin/sh -ex
+
+discard_file=/dev/shm/postfix.discard
+
+write_discard() {
+	patt=$1
+	ext=$2
+
+	file=$discard_file$ext
+
+	test -z "$patt" && return
+	grep "$patt" $file && return
+
+	echo "## write_discard $patt [$ext]"
+	echo "/$patt/	DISCARD" | tee -a $file
+
+	./hold-remove-grep.sh $patt
+}
+
+mailq | grep '^[0-9A-F]' | sort -k 7 | grep \! | while read line ; do
+
+	id=$( echo $line | cut -d\! -f1 )
+
+	echo "## id $id [$line]"
+	id_file=/dev/shm/id.$id
+
+	sudo find /var/spool/postfix/ -name $id | sudo xargs -i postcat {} > $id_file
+	reply_to=$( grep '^Reply-To: .*' $id_file | cut -d: -f2 )
+	if [ ! -z "$reply_to" ] ; then
+		write_discard "$reply_to" # quote to pass as signle arg
+		echo $id | tee $discard_file.id
+
+	else
+		less $id_file
+		echo -n "ENTER anything to discard "
+		read do_discard < /dev/stdin
+		test ! -z "$do_discard" && write_discard "$do_discard" ".body"
+	fi
+
+
+
+done
