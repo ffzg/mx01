fdf27592b715b26d6e6c92df614ab1de9e5dc945
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-11-22 16:33:06 +0100

    update

--- unhold-body.sh
+++ unhold-body.sh
@@ -1,8 +1,11 @@
 #!/bin/sh -e
 
+# unhold-body.sh
+
 sudo id >/dev/null
 mailq | grep '^[0-9A-Z]*!' | awk '{ print $1 }' | sed 's/!$//' | while read id ; do
 	#echo "### $id ###"
 	sudo find /var/spool/postfix/ -name $id | sudo xargs -i postcat {} | sed "s/^/$id /" | \
-		grep -E '(paper abstract|abstract submission)' | cut -d' ' -f1 | sudo postsuper -H -
+		grep -i -E '(paper abstract|abstract submission|najava edukacije|esteem delegates|Hrvatska kulturna zajednica|List-Post:|List-Unsubscribe:|pozvati Vas|znanstvenu konferenciju|akademskog|University|article|izlet|studentice|studenti|predavanje|Professor|Radionica|akademici|conference|Pozivnica|izlet|domjen|vrtic|seminar|Predstava|letak|roditelji|sastanku|roditelje|evidenciji|knjig|diplome|nagrade|unizg.hr|kgz.hr)' \
+		| cut -d' ' -f1 | sudo postsuper -H -
 done
--- unhold-lists.sh
+++ unhold-lists.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -e
 
+# unhold-lists.sh
+
 # -B 1 to support to address from mailq
 
 mailq \
-	| grep -B 1 -E '(drupaladmin@mail.h-net.org|owner-museum-l@HOME.EASE.LSOFT.COM|visionesdelofantastico@gmail.com|@hotmail.com|jtomas@ffzg.hr|bounce@client.scholargatherings.com|owner-digital-preservation@JISCMAIL.AC.UK|info.icarushr@gmail.com|owner-infoling@LISTSERV.REDIRIS.ES|bounce-mc|@mailer.opensciencejournals.net|bounce.linkedin.com|sender[0-9].acmbma.com|noreply@esavjetovanja.gov.hr|socinfolista@gmail.com|=astulhof=ffzg.hr@gmail.com|veda-nada@knjiga-znanja-veda.hr|istrazivanje.udruge@gmail.com)' \
+	| grep -B 1 -E '(drupaladmin@mail.h-net.org|owner-museum-l@HOME.EASE.LSOFT.COM|visionesdelofantastico@gmail.com|@hotmail.com|jtomas@ffzg.hr|bounce@client.scholargatherings.com|owner-digital-preservation@JISCMAIL.AC.UK|info.icarushr@gmail.com|owner-infoling@LISTSERV.REDIRIS.ES|bounce-mc|@mailer.opensciencejournals.net|bounce.linkedin.com|sender[0-9].acmbma.com|noreply@esavjetovanja.gov.hr|socinfolista@gmail.com|=astulhof=ffzg.hr@gmail.com|veda-nada@knjiga-znanja-veda.hr|istrazivanje.udruge@gmail.com|ffrz.hr|donotreply@fer.hr|knjiznica.m.j.zagorke@kgz.hr|myess@upf.edu|secretariat@wcprome2024.com|mail.data.europa.eu|@fuds.si|vedrancosic@gmail.com|shisecond@shiconference.site|prejimsfoundation@gmail.com|matica.cakovec@gmail.com|blackouthiphop@gmail.com|icadl2022@gmail.com|dr.stambuk@gmail.com|callforpaper@ijlrhss.com|usf.ffzg@gmail.com|socinfolista@gmail.com|pisjournal.net@gmail.com|hsls.zagreb@gmail.com|mandalenal@gmail.com)' \
 	| grep '^[0-9A-F]*!' \
 	| awk '{ print $1 }' | tr -d '!' | xargs -i postsuper -H {}
 sendmail -q
