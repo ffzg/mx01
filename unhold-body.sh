2224be3ef2b5c92e155eff35925cd16ce1b1d30c
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-11-24 18:28:00 +0100

    require space before pattern
    so we don't have false positive matches in base64 encoded files

--- unhold-body.sh
+++ unhold-body.sh
@@ -6,6 +6,6 @@ sudo id >/dev/null
 mailq | grep '^[0-9A-Z]*!' | awk '{ print $1 }' | sed 's/!$//' | while read id ; do
 	#echo "### $id ###"
 	sudo find /var/spool/postfix/ -name $id | sudo xargs -i postcat {} | sed "s/^/$id /" | \
-		grep -i -E '(paper abstract|abstract submission|najava edukacije|esteem delegates|Hrvatska kulturna zajednica|List-Post:|List-Unsubscribe:|pozvati Vas|znanstvenu konferenciju|akademskog|University|article|izlet|studentice|studenti|predavanje|Professor|Radionica|akademici|conference|Pozivnica|izlet|domjen|vrtic|seminar|Predstava|letak|roditelji|sastanku|roditelje|evidenciji|knjig|diplome|nagrade|unizg.hr|kgz.hr)' \
+		grep --color -i -E ' (paper abstract|abstract submission|najava edukacije|esteem delegates|Hrvatska kulturna zajednica|List-Post:|List-Unsubscribe:|pozvati Vas|znanstvenu konferenciju|akademskog|University|article|izlet|studentice|studenti|predavanje|Professor|Radionica|akademici|conference|Pozivnica|izlet|domjen|vrtic|seminar|Predstava|letak|roditelji|sastanku|roditelje|evidenciji|knjig|diplome|nagrade|unizg.hr|kgz.hr)' \
 		| cut -d' ' -f1 | sudo postsuper -H -
 done
