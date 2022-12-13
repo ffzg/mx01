20131153d021e9ba96baa6bdc46e4b5afe2a25f2
FFZG admin <admin@ffzg.hr>
2022-12-13 18:37:15 +0100

    use regex/unhold-from

--- tail-unhold-lists.sh
+++ tail-unhold-lists.sh
@@ -6,7 +6,7 @@
 
 tail -F /var/log/mail.log  \
 	| grep ': [0-9A-F]*: hold:' \
-	| grep -B 1 -E '(drupaladmin@mail.h-net.org|owner-museum-l@HOME.EASE.LSOFT.COM|visionesdelofantastico@gmail.com|@hotmail.com|jtomas@ffzg.hr|bounce@client.scholargatherings.com|owner-digital-preservation@JISCMAIL.AC.UK|info.icarushr@gmail.com|owner-infoling@LISTSERV.REDIRIS.ES|bounce-mc|@mailer.opensciencejournals.net|bounce.linkedin.com|sender[0-9].acmbma.com|noreply@esavjetovanja.gov.hr|socinfolista@gmail.com|=astulhof=ffzg.hr@gmail.com|veda-nada@knjiga-znanja-veda.hr|istrazivanje.udruge@gmail.com|ffrz.hr|donotreply@fer.hr|knjiznica.m.j.zagorke@kgz.hr|myess@upf.edu|secretariat@wcprome2024.com|mail.data.europa.eu|@fuds.si|vedrancosic@gmail.com|shisecond@shiconference.site|prejimsfoundation@gmail.com|matica.cakovec@gmail.com|blackouthiphop@gmail.com|icadl2022@gmail.com|dr.stambuk@gmail.com|callforpaper@ijlrhss.com|usf.ffzg@gmail.com|socinfolista@gmail.com|pisjournal.net@gmail.com|hsls.zagreb@gmail.com|mandalenal@gmail.com|info.icarushr@gmail.com|service@intl.paypal.com)' \
+	| grep -B 1 -E -f regex/unhold-from \
 	| tee /dev/stderr \
 	| cut -d' ' -f6 | tr -d ':' \
 	| xargs -i postsuper -H {}
