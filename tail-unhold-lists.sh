270950db45f9724596d6ecf6ea0962842b8cd923
FFZG admin <admin@ffzg.hr>
2023-03-11 14:39:59 +0100

    simplify unhold lists script

--- tail-unhold-lists.sh
+++ tail-unhold-lists.sh
@@ -1,27 +1,10 @@
 #!/bin/sh -e
 
-# unhold-lists.sh
-
-# -B 1 to support to address from mailq
+# tail-unhold-lists.sh
 
 cd /home/dpavlin/mx01
 
 tail -F /var/log/mail.log  \
 	| grep --line-buffered ': [0-9A-F]*: hold:' \
-	| xargs -i sh -cx './unhold-lists.sh ; ./unhold-body.sh'
-		
-done
+	| xargs -i sh -cx './unhold-body.sh ; ./unhold-lists.sh'
 
-exit 0
-tail -F /var/log/mail.log  \
-	| grep ': [0-9A-F]*: hold:' \
-	| tee /dev/stderr \
-	| grep -B 1 -E -f regex/unhold-from \
-	| tee /dev/stderr \
-	| cut -d' ' -f6 | tr -d ':' \
-	| xargs -i postsuper -H {}
-
-exit 0
-	| grep '^[0-9A-F]*!' \
-	| awk '{ print $1 }' | tr -d '!' | xargs -i postsuper -H {}
-sendmail -q
