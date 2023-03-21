486dcd39eb73ec346fac9d988c1c8bc3b3e13e2b
FFZG admin <admin@ffzg.hr>
2023-03-21 12:12:56 +0100

    collect full messages in /dev/shm/id.xxxxx

--- hold2mbox.sh
+++ hold2mbox.sh
@@ -11,9 +11,9 @@ id=$( echo $line | awk '{ print $1 }' | tr -d '!' );
 echo $id >> $mbox.ids
 queue_file=$( sudo find /var/spool/postfix/ -name $id )
 echo -n "From $id " >> $mbox
-postcat -e $queue_file > /dev/shm/hold.e
-grep message_arrival_time: /dev/shm/hold.e | cut -d: -f2- >> $mbox
-postcat -hb $queue_file >> $mbox
+postcat -e $queue_file > /dev/shm/id.$id
+grep message_arrival_time: /dev/shm/id.$id | cut -d: -f2- >> $mbox
+postcat -hb $queue_file | tee -a /dev/shm/id.$id >> $mbox
 echo >> $mbox
 
 done
