9b1866e63c338a16c4bcabce93e2aa2713f0de02
FFZG admin <admin@ffzg.hr>
2023-04-02 16:23:24 +0200

    fix search pattern

--- mailq-to-id.sh
+++ mailq-to-id.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -xe
 
 test -z "$1" && echo "Usage: $0 to-email@example.com" && exit 1
-mailq | grep -B 2 anita_ruso@yahoo.com | tee /dev/stderr | grep '^[0-9A-F]' | awk '{ print $1 }'
+mailq | grep -B 2 $1 | tee /dev/stderr | grep '^[0-9A-F]' | awk '{ print $1 }' | tr -d \!
 
