b5fdc1f877525f802ecda00119276866981c6f14
FFZG admin <admin@ffzg.hr>
2023-04-05 10:11:18 +0200

    remove space before pattern for regex/unhold-body-exact

--- unhold-body.sh
+++ unhold-body.sh
@@ -5,8 +5,8 @@
 sudo id >/dev/null
 mailq | grep '^[0-9A-Z]*!' | awk '{ print $1 }' | sed 's/!$//' | while read id ; do
 	#echo "### $id ###"
-	sudo find /var/spool/postfix/ -name $id | sudo xargs -i postcat {} | sed "s/^/$id /" | \
-		grep -i -E -f regex/unhold-body \
+	sudo find /var/spool/postfix/ -name $id | sudo xargs -i postcat -hbe {} | sed "s/^/$id /" | \
+		grep -i -E -f regex/unhold-body -f regex/unhold-body-exact \
 		| tee /dev/stderr \
 		| cut -d' ' -f1 | sudo postsuper -H -
 done
